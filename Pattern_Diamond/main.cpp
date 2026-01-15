1c3076ab60303b4d90e5e65080154aac8f7d97dd
Gnan Parekh <gnan0839@gmail.com>
2026-01-15 12:34:26 +0530

    Create  main.cpp

---
+++ Pattern_Diamond/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cout << "Enter size of diamond: ";
+    cin >> n;
+
+    // Upper half
+    for(int i = 1; i <= n; i++) 
+    {
+        for(int j = i; j < n; j++) 
+            cout << " ";
+        for(int j = 1; j <= 2*i - 1; j++) 
+            cout << "*";
+        cout << endl;
+    }
+
+    // Lower half
+    for(int i = n - 1; i >= 1; i--) 
+    {
+        for(int j = n; j > i; j--) 
+            cout << " ";
+        for(int j = 1; j <= 2*i - 1; j++) 
+            cout << "*";
+        cout << endl;
+    }
+
+    return 0;
+}
