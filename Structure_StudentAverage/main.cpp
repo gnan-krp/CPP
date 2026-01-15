8527299a1836263a8d8e4143439a12995622f3c4
Gnan Parekh <gnan0839@gmail.com>
2026-01-15 13:11:43 +0530

    Update main.cpp

--- Structure_StudentAverage/main.cpp
+++ Structure_StudentAverage/main.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 using namespace std;
 
-struct Student 
+struct Student
 {
-    string name;
-    int marks[3];
+    int marks[5];
 };
 
-int main() 
+int main()
 {
     Student s;
-    cout << "Enter student name: ";
-    cin >> s.name;
-
-    cout << "Enter marks in 3 subjects: ";
-    for(int i = 0; i < 3; i++)
-        cin >> s.marks[i];
-
     int sum = 0;
-    for(int i = 0; i < 3; i++)
+
+    cout << "Enter 5 subject marks: ";
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> s.marks[i];
         sum += s.marks[i];
+    }
+
+    float average = sum / 5.0;
+
+    cout << "Average Marks = " << average << endl;
+
+    return 0;
+}
